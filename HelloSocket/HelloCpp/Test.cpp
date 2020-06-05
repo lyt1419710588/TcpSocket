84a41647510dfe1447f800fcfbdb73886b6f3cb4
lyt <14197410588@qq.com>
2020-06-05 21:33:33 +0800

    测试使用function与lambda表达式

---
+++ HelloSocket/HelloCpp/Test.cpp
@@ -0,0 +1,25 @@
+#include <functional>
+//function
+//lambda
+
+int funA(int a, int b)
+{
+	printf("a + b = %d\n", a + b);
+	return a + b;
+}
+
+int main()
+{
+	/*std::function<int(int, int)> call = funA;
+	int c = call(10, 5);*/
+
+	std::function<int(int, int)> call;
+
+	//lambda±í´ïÊ½
+	int n = 5;
+	call = [n](int a, int b) -> int {
+		return 10;
+	};
+	int n = call(5, 6);
+	return 0;
+}
