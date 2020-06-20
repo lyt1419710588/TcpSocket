5391c4d0c177083b5824b1c313929cbe43a60038
lyt <14197410588@qq.com>
2020-06-20 18:20:58 +0800

    增加字节流写入和读取类

--- HelloSocket/HelloSocket/Test.cpp
+++ HelloSocket/HelloSocket/Test.cpp
@@ -1,16 +1,71 @@
-#define WIN32_LEAN_AND_MEAN
+#include "EasyTcpClient.hpp"
 
-#include <Windows.h>
-#include <WinSock2.h>
+#include "CELLStream.hpp"
 
-
-
+class MyClient :public EasyTcpClient
+{
+public:
+	void OnNetMsg(DataHeader* header)
+	{
+		//处理客户端请求
+		switch (header->cmd)
+		{
+		case CMD_LOGIN_RESULT:
+		{
+			//接收服务器返回的数据
+			LoginResult *ret = (LoginResult*)header;
+			// CELLLog::Info("收到服务端消息：retLogin = %d，数据长度:%d\n", ret->result, header->dataLength);
+		}
+		break;
+		case CMD_ERROR:
+		{
+			//接收服务器返回的数据
+			CELLLog::Info("收到服务端消息：CMD_ERROR  sock = %d，数据长度:%d\n", _pClient->getSocket(), header->dataLength);
+		}
+		break;
+		default:
+		{
+			CELLLog::Info("收到服务端未定义数据，  sock = %d，数据长度:%d\n", _pClient->getSocket(), header->dataLength);
+		}
+		break;
+		}
+	}
+};
 int  main()
 {
-	WORD ver = MAKEWORD(2, 2);
-	WSADATA data;
-	WSAStartup(ver, &data);
+	CELLStream s;
 
-	WSACleanup();
+	s.writeInt8(15);
+	s.writeInt16(15);
+	s.writeInt32(15);
+	s.writeFloat(15.333f);
+	s.writeDouble(15.333f);
+	char* strName = "laoli";
+	s.writeArray(strName,strlen(strName));
+
+	int pas[5] = {1,2,3,4,5};
+	s.writeArray(pas, 5);
+
+	auto s1 = s.readInt8();
+	auto s2 = s.readInt16();
+	auto s3 = s.readInt32();
+	auto s4 = s.readFloat();
+	auto s5 = s.readDouble();
+
+
+	char strName1[10] = {};
+	s.ReadArray(strName1, 10);
+
+	int pas1[10] = {};
+	s.ReadArray(pas1, 10);
+
+
+	MyClient client;
+	client.Connect("127.0.0.1", 4567);
+	while (client.isRun())
+	{
+		client.OnRun();
+		CELLThread::Sleep(500);
+	}
 	return 0;
 }
---
+++ HelloSocket/depend/include/CELLStream.hpp
@@ -0,0 +1,189 @@
+#ifndef _CELL_STAREAM_HPP_
+#define _CELL_STAREAM_HPP_
+#include <cstdint>
+
+class CELLStream
+{
+public:
+	CELLStream(int nSize = 1024)
+	{
+		_nSize = nSize;
+		_pBuff = new char[_nSize];
+	}
+
+	CELLStream(char* pData,int nSize = 1024,bool bDel = false)
+	{
+		_bDelete = bDel;
+		_nSize = nSize;
+		_pBuff = pData;
+	}
+	~CELLStream()
+	{
+		if (_pBuff && _bDelete)
+		{
+			delete[] _pBuff;
+			_pBuff = nullptr;
+		}
+	}
+private:
+	inline bool canRead(uint32_t n)
+	{
+		return _nSize - _nReadPos >= n;
+	}
+	inline bool canWrite(uint32_t n)
+	{
+		return _nSize - _nWritePos >= n;
+	}
+	inline void push(uint32_t n)
+	{
+		_nWritePos += n;
+	}
+
+	inline void pop(uint32_t n)
+	{
+		_nReadPos += n;
+	}
+public:
+	///read
+	template<typename T>
+	bool Read(T& ret,bool bOffset = true)
+	{
+		auto nLen = sizeof(T);
+		if (canRead(nLen))
+		{
+			memcpy(&ret, _pBuff + _nReadPos, nLen);
+			if (bOffset)
+			{
+				pop(nLen);
+			}
+			return true;
+		}
+		return false;
+	}
+	template<typename T>
+	bool onlyRead(T &n)
+	{
+		return Read(n, false);
+	}
+	//读取数组
+	template<typename T>
+	uint32_t ReadArray(T *pData,uint32_t  nLen)
+	{
+		//读取数组元素个数
+		uint32_t tLen = 0;
+		//读取数据长度，但不便宜读取位置
+		Read(tLen,false);
+		if (tLen < nLen)
+		{
+			auto tSize = tLen * sizeof(T);
+			if (canRead(tSize + sizeof(uint32_t)))
+			{
+				pop(sizeof(uint32_t));
+				memcpy(pData, _pBuff + _nReadPos, tSize);
+				pop(tSize);
+				return tLen;
+			}
+		}
+		return 0;
+	}
+	////read
+	int8_t  readInt8()
+	{
+		int8_t n = 0;
+		Read(n);
+		return n;
+	}
+	int16_t readInt16()
+	{
+		int16_t n = 0;
+		Read(n);
+		return n;
+	}
+	int32_t readInt32()
+	{
+		int32_t n = 0;
+		Read(n);
+		return n;
+	}
+	float   readFloat()
+	{
+		float n = 0.0f;
+		Read(n);
+		return n;
+	}
+	double  readDouble()
+	{
+		double n = 0.0f;
+		Read(n);
+		return n;
+	}
+
+	
+
+
+	////write
+	bool writeInt8(int8_t n)
+	{
+		return Write(n);
+	}
+
+	bool writeInt16(int16_t n)
+	{
+		return Write(n);
+	}
+	
+	bool writeInt32(int32_t n)
+	{
+		return Write(n);
+	}
+
+	bool writeFloat(float n)
+	{
+		return Write(n);
+	}
+	bool writeDouble(double n)
+	{
+		return Write(n);
+	}
+
+	template<typename T>
+	bool writeArray(T *pData, uint32_t nLen)
+	{
+		auto aLen = sizeof(T) * nLen;
+		if (canWrite(aLen + sizeof(uint32_t)))
+		{
+			writeInt32(nLen);
+			memcpy(_pBuff + _nWritePos, pData, aLen);
+			push(aLen);
+			return true;
+		}
+		return false;
+	}
+	
+	template<typename T>
+	bool Write(T n)
+	{
+		auto nLen = sizeof(T);
+		if (canWrite(nLen))
+		{
+			memcpy(_pBuff + _nWritePos, &n, nLen);
+			push(nLen);
+			return true;
+		}
+		return false;
+	}
+private:
+	//数据缓冲区
+	char* _pBuff = nullptr;
+	//缓冲区的大小
+	int _nSize = 0;
+
+	//已读取的数据尾部位置
+	int _nReadPos = 0;
+	//已写到的数据尾部位置
+	int _nWritePos = 0;
+	//缓冲区是否应该被释放
+	bool _bDelete = true;
+
+};
+#endif // !_CELL_STAREAM_HPP_
